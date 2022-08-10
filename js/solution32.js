856735f632dad0c2d066e8687a36e9316b62cdea
thahseen53 <thahseen53@gmail.com>
2022-08-10 19:29:36 +0530

    solution 32 added

---
+++ js/solution32.js
@@ -0,0 +1,13 @@
+//returning 4 if n is 7 returning 7 if n is 4 returning 0 if n is nor 4 niether 7 
+//without using if ,switch ,ternary operation
+
+
+function fourSeven(n){
+   return fourSeven.num[n] || fourSeven.num['default']
+  }
+  
+   fourSeven.num ={
+     4:7,
+     7:4,
+     default:0
+   }
